38d5cbeb863baecf9e0a5a3e05b7d6c97f693366
dumi994 <dumi.9944@gmail.com>
2021-04-29 17:16:54 +0200

    la macchina sceglie chi ha vinto

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -2,32 +2,34 @@
 var pariDispari = prompt('Scegli pari o dispari')
 var numUtente = Number(prompt('Scegli un numero da 1 a 5'))
 
-console.log(`${pariDispari} ${numUtente}`)
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
 function numCasuale(min, max) {
     return Math.floor(Math.random() *5 + 1);
 }
-console.log(numCasuale(1, 5));
 //Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 function somma (numUtente, numCasuale){
+    console.log(numUtente, numCasuale);
     var risultato = numUtente + numCasuale;
-    if (risultato % 2 == 0){
-        console.log('pari');
-    }else{
-        console.log('dispari');
-    }
-    
-
-    /* if (numCasuale + numUtente % 2 == 0){
-        console.log('pari');
-    }else{
-        console.log('dispari');
-    } */
-    return risultato;
+   
     console.log(risultato);
+    return risultato;
 }
-somma (numUtente, numCasuale(1, 5));
+
+var risultato = somma (numUtente, numCasuale(1, 5));
+if (risultato % 2 == 0){
+    var sommaPariDispari = 'pari';
+    console.log('pari');
+}else{
+    var sommaPariDispari = 'dispari';
+    console.log('dispari');
+}
 
 //Dichiariamo chi ha vinto.
+
+if (pariDispari == sommaPariDispari){
+    console.log('hai vinto');
+}else{
+    console.log('hai perso');
+}
