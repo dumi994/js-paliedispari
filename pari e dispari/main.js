621fe1470d8394eb07bd5e06f69afbdcf40c3a63
dumi994 <dumi.9944@gmail.com>
2021-04-29 16:15:41 +0200

    il programma dichiara il risultato

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -6,16 +6,21 @@ console.log(`${pariDispari} ${numUtente}`)
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
 function numCasuale(min, max) {
-    return Math.floor(Math.random() *5);
+    return Math.floor(Math.random() *5 + 1);
 }
 console.log(numCasuale(1, 5));
-
-/* function getRandomInteger (min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
-}
-
-console.log(getRandomInteger(10, 45)); */
 //Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
+/* function somma (numUtente, numCasuale){
+    var risultato = numUtente + numCasuale;
+    console.log(risultato);
+    return risultato;
+}
+somma = (numUtente, numCasuale); */
 
+if (numCasuale + numUtente % 2 == 0){
+    console.log('pari');
+}else{
+    console.log('dispari');
+}
 //Dichiariamo chi ha vinto.
